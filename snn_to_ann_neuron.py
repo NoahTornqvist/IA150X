6ddd501cc49c53203605343cc2c9f7092e05a3bf
Brian Onoszko <brian2002@live.se>
2025-08-21 14:06:12 +0200

    License and copyright changes
    Added license and SNUCell header

--- snn_to_ann_neuron.py
+++ snn_to_ann_neuron.py
@@ -1,3 +1,26 @@
+# Copyright (c) 2020 IBM Research. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========================================================================
+# Modified by Brian Onoszko and Noah Törnqvist on 2025-08-21:
+# - Updated the code to work for Python version 3.10.11
+# - Rewrote StepFunction to utilize quantization supported operators for STEdgeAI.
+# - Rewrote call to utilize quanitzation supported operators for STEdgeAI.
+# - Rewrote build to work with unrolling code
+# - Removed StateTuple as it was no longer required.
+# - Removed backward pass.
+# - Removed recurrent spiking.
+
 import tensorflow as tf
 
 def StepFunction(x):
